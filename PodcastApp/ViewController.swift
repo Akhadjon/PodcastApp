65510b99a2a600392fb1c8b5be5f6c1929767ba7
Akhadjon Abdukhalilov <abduxalilovahadjon97@gmail.com>
2020-11-24 20:18:03 +0500

    Initial Commit

---
+++ PodcastApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodcastApp
+//
+//  Created by Akhadjon Abdukhalilov on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
